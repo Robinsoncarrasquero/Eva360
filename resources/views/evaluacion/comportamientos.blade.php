c71fd3d89d9b4b1283dad6e21516ff7bc55221ad
Robinson Carrasquero <robinson.carrasquero@gmail.com>
2022-01-28 21:49:05 -0400

    Fix view comportamientos

--- resources/views/evaluacion/comportamientos.blade.php
+++ resources/views/evaluacion/comportamientos.blade.php
@@ -5,109 +5,108 @@
 @section('content')
 
 <div class="container">
+
     <div id="flash-message">
         @include('flash-message')
     </div>
 
-
-
-
-    <div class="card-header mb-3">
-        <h5 class="text text-center">Estimado evaluador {{  $evaluacion->evaluador->name }}, analice con criterio y determinacion las competencia de</h5>
-        <h4 class="text text-danger d-flex justify-content-center">{{ $evaluacion->evaluador->evaluado->name }}</h4>
-    </div>
-
-    <div class="mb-2">
-        <h4 class="text text-center mt-2 border-primary "><strong> {{ $evaluacion->competencia->name }}</strong></h4>
-        <h6 class="card card-header description" >{{ $evaluacion->competencia->description}}</h6>
-    </div>
-
     <div class="clearfix">
-    @if ($evaluacion->competencia->grados->isNotEmpty())
-        <form action="{{ route('evaluacion.store',$evaluacion) }}" method="POST" id="form-select">
-        @csrf
-            <div class="table ">
-                <table class="table  table-striped table-table">
-                    <thead class="table-preguntas text-dark">
-                        {{-- <th scope="col">#</th> --}}
-                        <th scope="col">Comportamiento observado</th>
-                        <th scope="col">Seleccion</th>
-                        <th scope="col">Frecuencia</th>
-                    </thead>
-                    <tbody>
 
-                    @foreach ($evaluacion->comportamientos as $comportamiento)
-                        <tr data-id="{{"$comportamiento->id"}}" >
-                            {{-- <th scope="row">{{ $comportamiento->grado_id }}</th> --}}
-                            <td>{{$comportamiento->grado->description}}</td>
-                            <td >
-                                <div class="form-check">
-                                    <label class="form-check-label " for="gradocheck[]"></label>
-                                    @if ($evaluacion->competencia->seleccionmultiple)
-                                        <input type="checkbox" class="no-check-select " id="{{"radiogrado$comportamiento->id"}}" name="gradocheck[]" value="{{"$comportamiento->id"}}"
-                                        disabled checked
-                                        @if ($evaluacion->evaluador->status==2) disabled  @endif >
-                                    @else
-                                        <input type="checkbox" class="check-select "  id="{{"radiogrado$comportamiento->id"}}" name="gradocheck[]" value="{{"$comportamiento->id"}}"
-                                        @if($comportamiento->frecuencia) checked @endif
-                                        @if ($evaluacion->evaluador->status==2) disabled  @endif>
-                                    @endif
-                                </div>
-                            </td>
-                            <td >
-                                @foreach ($frecuencias  as $frecuencia)
-                                <div class="xform-check d-flex justify-content-between">
-                                    <label for="frecuenciacheck[]" class="xform-check-label">{{ $frecuencia->name}}</label>
-                                    @if ($evaluacion->competencia->seleccionmultiple)
-                                        <input  type="radio" class="no-radiofrecuencia" id="{{"radiofrecuencia$comportamiento->grado_id"}}"
-                                        value="{{"$comportamiento->id,$frecuencia->id"}}" name="frecuenciacheck[{{ $comportamiento->grado_id }}]"
-                                        @if ($comportamiento->frecuencia===$frecuencia->valor) checked @endif
-                                        @if ($evaluacion->evaluador->status==2) disabled  @endif>
-                                    @else
-                                        <input type="radio" class="radiofrecuencia" id="{{"radiofrecuencia$comportamiento->grado_id"}}"
-                                        value="{{"$comportamiento->id,$frecuencia->id"}}" name="frecuenciacheck[{{ $comportamiento->grado_id }}]"
-                                        @if ($comportamiento->frecuencia===$frecuencia->valor) checked @endif
-                                        @if ($evaluacion->evaluador->status==2) disabled  @endif>
-                                    @endif
-                                </div>
-                                @endforeach
-                            </td>
-                        </tr>
-                    @endforeach
+        <div class="card-header mb-3">
+            <h5 class="text text-center">Estimado evaluador {{  $evaluacion->evaluador->name }}, analice con criterio y determinacion las competencia de</h5>
+            <h4 class="text text-danger d-flex justify-content-center">{{ $evaluacion->evaluador->evaluado->name }}</h4>
+        </div>
 
-                    </tbody>
-                </table>
+        <div class="mb-2">
+            <h4 class="text text-center mt-2 border-primary "><strong> {{ $evaluacion->competencia->name }}</strong></h4>
+            <h6 class="card card-header description" >{{ $evaluacion->competencia->description}}</h6>
+        </div>
 
-            </div>
 
-            <div class="col-md-4 ">
-                <div id="divtodo">
+        @if ($evaluacion->competencia->grados->isNotEmpty())
+            <form action="{{ route('evaluacion.store',$evaluacion) }}" method="POST" id="form-select">
+            @csrf
+                <div class="table ">
+                    <table class="table  table-striped table-table">
+                        <thead class="table-preguntas text-dark">
+                            {{-- <th scope="col">#</th> --}}
+                            <th scope="col">Comportamiento observado</th>
+                            <th scope="col">Seleccion</th>
+                            <th scope="col">Frecuencia</th>
+                        </thead>
+                        <tbody>
+
+                        @foreach ($evaluacion->comportamientos as $comportamiento)
+                            <tr data-id="{{"$comportamiento->id"}}" >
+                                {{-- <th scope="row">{{ $comportamiento->grado_id }}</th> --}}
+                                <td>{{$comportamiento->grado->description}}</td>
+                                <td >
+                                    <div class="form-check">
+                                        <label class="form-check-label " for="gradocheck[]"></label>
+                                        @if ($evaluacion->competencia->seleccionmultiple)
+                                            <input type="checkbox" class="no-check-select " id="{{"radiogrado$comportamiento->id"}}" name="gradocheck[]" value="{{"$comportamiento->id"}}"
+                                            disabled checked
+                                            @if ($evaluacion->evaluador->status==2) disabled  @endif >
+                                        @else
+                                            <input type="checkbox" class="check-select "  id="{{"radiogrado$comportamiento->id"}}" name="gradocheck[]" value="{{"$comportamiento->id"}}"
+                                            @if($comportamiento->frecuencia) checked @endif
+                                            @if ($evaluacion->evaluador->status==2) disabled  @endif>
+                                        @endif
+                                    </div>
+                                </td>
+                                <td >
+                                    @foreach ($frecuencias  as $frecuencia)
+                                    <div class="xform-check d-flex justify-content-between">
+                                        <label for="frecuenciacheck[]" class="xform-check-label">{{ $frecuencia->name}}</label>
+                                        @if ($evaluacion->competencia->seleccionmultiple)
+                                            <input  type="radio" class="no-radiofrecuencia" id="{{"radiofrecuencia$comportamiento->grado_id"}}"
+                                            value="{{"$comportamiento->id,$frecuencia->id"}}" name="frecuenciacheck[{{ $comportamiento->grado_id }}]"
+                                            @if ($comportamiento->frecuencia===$frecuencia->valor) checked @endif
+                                            @if ($evaluacion->evaluador->status==2) disabled  @endif>
+                                        @else
+                                            <input type="radio" class="radiofrecuencia" id="{{"radiofrecuencia$comportamiento->grado_id"}}"
+                                            value="{{"$comportamiento->id,$frecuencia->id"}}" name="frecuenciacheck[{{ $comportamiento->grado_id }}]"
+                                            @if ($comportamiento->frecuencia===$frecuencia->valor) checked @endif
+                                            @if ($evaluacion->evaluador->status==2) disabled  @endif>
+                                        @endif
+                                    </div>
+                                    @endforeach
+                                </td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
 
                 </div>
+
+                <div class="col-md-4 ">
+                    <div id="divtodo">
+
+                    </div>
+                </div>
+
+
+                <div class="clearfix">
+                    @if (Auth::user()->admin())
+                        {{-- <span class="float-left"><a href="{{ url()->previous() }}" class="btn btn-dark btn-lg">Regresar</a></span> --}}
+                    @else
+                        <span class="float-left"><a href="{{ route('evaluacion.competencias',$evaluacion->evaluador->id) }}" class="btn btn-dark btn-lg">Regresar</a></span>
+                        @if ($evaluacion->evaluador->status!=2)
+                            <button type="submit" class="btn btn-dark btn-lg float-right" value="Next" >Guardar</button>
+                        @endif
+                    @endif
+                    {{-- <span class="float-left"><a href="{{ url()->previous() }}" class="btn btn-dark btn-lg">Back</a></span> --}}
+
+                </div>
+
+            </form>
+
+        @else
+            <div class="col-md-12">
+                <p>No hay datos disponible para evaluacion</p>
             </div>
+        @endif
 
-
-        <div class="clearfix">
-            @if (Auth::user()->admin())
-                {{-- <span class="float-left"><a href="{{ url()->previous() }}" class="btn btn-dark btn-lg">Regresar</a></span> --}}
-            @else
-                <span class="float-left"><a href="{{ route('evaluacion.competencias',$evaluacion->evaluador->id) }}" class="btn btn-dark btn-lg">Regresar</a></span>
-                @if ($evaluacion->evaluador->status!=2)
-                    <button type="submit" class="btn btn-dark btn-lg float-right" value="Next" >Guardar</button>
-                 @endif
-            @endif
-            {{-- <span class="float-left"><a href="{{ url()->previous() }}" class="btn btn-dark btn-lg">Back</a></span> --}}
-
-
-        </div>
-
-    </form>
-
-    @else
-        <div class="col-md-12">
-            <p>No hay datos disponible para evaluacion</p>
-        </div>
-    @endif
     </div>
 
 </div>
