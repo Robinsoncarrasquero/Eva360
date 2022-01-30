42ee779b761709bd6ee6a25d52ee2ea7ce84c0ef
Robinson Carrasquero <robinson.carrasquero@gmail.com>
2022-01-30 12:55:35 -0400

    Update show competencias evaluador

--- resources/views/evaluacion/competencias.blade.php
+++ resources/views/evaluacion/competencias.blade.php
@@ -19,38 +19,43 @@
                     <h5 class="text text-danger d-flex justify-content-center">{{ $evaluado->name }}</h5>
             </div>
 
-            <div class="text text-center mt-3 text-success">
-                <h4>COMPETENCIAS</h4>
+            <div class="text text-center mt-3 text-dark">
+                <h4 >COMPETENCIAS</h4>
             </div>
 
         </div>
 
-        @if($competencias)
 
-                <form method="POST" action="{{ route('evaluacion.finalizar',$evaluador->id) }}">
+            @if($competencias)
+                    <form method="POST" action="{{ route('evaluacion.finalizar',$evaluador->id) }}">
                     @csrf
+                    <div class="row ">
 
                     @foreach($competencias as $competencia)
 
-                        <div class="card-header mt-3 mb-2">
-                            <div class="text text-center">
-                                <a href="{{route('evaluacion.responder', $competencia->id)}}" style="color: rgb(16, 17, 17)" >
-                                <h3 >{{$competencia->competencia->name}}</h3></a>
-                            </div>
+                    <div class ="col-sm-12 col-md-4">
 
-                            <div class="text text-center">
+                        <div class="card mt-4 pb-2  @if($competencia->resultado) border-success @else border-danger @endif" style="width:18rem;">
+                            {{-- <img class="card-img-top" src="..." alt="Card image cap"> --}}
+                            <div class="card-body">
+                                <h5 class="card-title">{{$competencia->competencia->name}}</h5>
+                                <p class="card-text">{{ substr($competencia->competencia->description,0,40) }}...</p>
+                                <a href="{{route('evaluacion.responder', $competencia->id)}}" style="color: rgb(16, 17, 17)" >
+
                                 @if($competencia->resultado)
-                                    <a href="{{route('evaluacion.responder', $competencia->id)}}" >
-                                        <span ><i class="material-icons text-success">thumb_up_alt</i></span></a>
-                                @else
-                                    <a href="{{route('evaluacion.responder', $competencia->id)}}" >
-                                    <span class="spinner-grow spinner-grow-sm text-danger align-center" role="status"><i class="material-icons spellcheck"></i></span></a>
-                                @endif
+                                        {{-- <span ><i class="material-icons text-success">thumb_up_alt</i></span></a> --}}
+                                        <a href="{{route('evaluacion.responder', $competencia->id)}}" class="btn btn-dark">Listo ya respondí</a>
+                                    @else
+                                    {{-- <span class="spinner-grow spinner-grow-sm text-danger align-center" role="status"><i class="material-icons spellcheck"></i></span></a> --}}
+                                    <a href="{{route('evaluacion.responder', $competencia->id)}}" class="btn btn-danger">Voy a responder</a>
+                                    @endif
+
                             </div>
                         </div>
+                    </div>
 
                     @endforeach
-
+                </div>
                     <div class="clearfix mt-3">
                         @if (Auth::user()->admin())
                             {{-- <span class="float-left"><a href="{{ url()->previous()}}" class="btn btn-dark btn-lg">Regresar</a></span> --}}
@@ -62,17 +67,17 @@
                         @endif
                     </div>
 
-                </form>
+                    </form>
+            @else
 
-        @else
+                <div class="alert-info">
+                    <p>No hay informacion disponibles para responder</p>
+                <div>
 
-            <div class="alert-info">
-                <p>No hay informacion disponibles para responder</p>
-            <div>
+            @endif
 
-        @endif
+            {{-- {{ $competencias->links() }} --}}
 
-        {{-- {{ $competencias->links() }} --}}
 
 </div>
 
